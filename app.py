ddcb790958d7d73d8a35af065d7282024fe2ddce
Mohit Nikumbh <nikumbhmohit@gmail.com>
2022-07-19 19:29:11 +0530

    updated

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "ci/cd pipeline created"
+
+if __name__== "__main__":
+    app.run(debug=True)
+    
